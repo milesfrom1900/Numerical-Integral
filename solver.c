d9762b2a1262026ad959cadf1e1b72df5e1f8210
quesswho <sebastianmiles@live.com>
2022-04-22 19:25:50 +0200

    Much faster integrator

--- solver.c
+++ solver.c
@@ -7,11 +7,14 @@ double function_f(double x) {
 }
 
 double solve_integral(double func (double), double a, double b) {
-	double sum = 0.0f, delta = 0.0001f, temp;
+	const double p1 = 0.000001f, p2 = 0.000002f;
+	double sum = 0.0f, delta = p1, temp;
 	while(a <= b) {
 		temp = func(a);
-		if(isfinite(temp))
-			sum += temp*delta;
+		if(isfinite(temp)) {
+			if(temp*delta > p2 || temp*delta < p1) delta = p1/temp;
+			sum += func(a)*delta;
+		}
 		a+=delta;
 	}
 	return sum;
@@ -19,7 +22,7 @@ double solve_integral(double func (double), double a, double b) {
 
 int main() {
 	double a = 0.0f;
-	double b = 1000.0f;
+	double b = 100000.0f;
 
 	printf("Evaluation: %.15f", solve_integral(&function_f, a, b));
 }
